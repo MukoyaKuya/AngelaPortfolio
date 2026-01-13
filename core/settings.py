afda8eb0da82bf007379e44a1c67e7888cde0446
Delton Mukoya Kuya <deltonmukoyakuya@gmail.com>
2026-01-14 01:26:47 +0300

    Fix local dev: Default DEBUG to True

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-c4uu$w_djdo6-vk1!*g8vdy$19^do#k*_rxjowqgy%j0z@-j_q')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
